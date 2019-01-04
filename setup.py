0f70716898531af267d2cb173ad7d3fdbb3770e7
petrochenko pavel <pavel@onpositive.com>
2019-01-04 23:33:38 +0700

    moving core dependency to 0.37

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='classification_pipeline',
-      version='0.34',
+      version='0.37',
       description='Declaraqtively configured pipeline for image segmentation',
       url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
       author='Petrochenko Pavel',
@@ -10,5 +10,5 @@ setup(name='classification_pipeline',
       packages=setuptools.find_packages(),
       include_package_data=True,
       dependency_links=['https://github.com/aleju/imgaug'],
-      install_requires=["musket_core>=0.35","segmentation_models"],
+      install_requires=["musket_core>=0.37","segmentation_models"],
       zip_safe=False)
