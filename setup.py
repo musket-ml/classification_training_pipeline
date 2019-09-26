d8d69ecf0e03d0de0bd69dd3bbf362cbd135eb4c
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-09-27 00:23:31 +0700

    updating pip version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='classification_pipeline',
-      version='0.38',
+      version='0.41',
       description='Declaraqtively configured pipeline for image segmentation',
       url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
       author='Petrochenko Pavel',
@@ -10,5 +10,5 @@ setup(name='classification_pipeline',
       packages=setuptools.find_packages(),
       include_package_data=True,
       dependency_links=['https://github.com/aleju/imgaug'],
-      install_requires=["musket_core>=0.38","segmentation_models"],
+      install_requires=["musket_core>=0.5","segmentation_models"],
       zip_safe=False)
