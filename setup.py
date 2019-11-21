bebf8a43ca3571a8aff4a78ebed31aa8a5a8f59a
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-11-21 10:00:36 +0700

    updating version number

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 import setuptools
+import os
+import io
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+    
 setup(name='classification_pipeline',
-      version='0.431',
-      description='Declaraqtively configured pipeline for image segmentation',
-      url='https://github.com/petrochenko-pavel-a/segmentation_training_pipeline',
+      version='0.432',
+      description='Declaratively configured pipeline for image classification',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/musket-ml/classification_training_pipeline',
       author='Petrochenko Pavel',
       author_email='petrochenko.pavel.a@gmail.com',
       license='MIT',
